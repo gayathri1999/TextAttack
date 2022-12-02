c680a4cc9696e08b88859c2f53b07194d9358c2e
gayathri <gayathrikimail@gmail.com>
2022-12-01 22:23:04 -0500

    fourth commit

--- textattack/attack_recipes/textfooler_jin_2019.py
+++ textattack/attack_recipes/textfooler_jin_2019.py
@@ -60,7 +60,7 @@ class TextFoolerJin2019(AttackRecipe):
         # results show that it's 0.5.)
         #
         constraints.append(WordEmbeddingDistance(min_cos_sim=0.5))
-        constraints.append(MaxWordsPerturbed(max_percent=0.01))
+        constraints.append(MaxWordsPerturbed(max_percent=0.75))
         #
         # Only replace words with the same part of speech (or nouns with verbs)
         #
