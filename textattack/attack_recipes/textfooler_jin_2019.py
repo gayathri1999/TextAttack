e45b09d076ae37cb3d00c3384d7ddd1a90b6a2a6
gayathri <gayathrikimail@gmail.com>
2022-12-05 15:16:07 -0500

    changing constraints

--- textattack/attack_recipes/textfooler_jin_2019.py
+++ textattack/attack_recipes/textfooler_jin_2019.py
@@ -57,8 +57,8 @@ class TextFoolerJin2019(AttackRecipe):
         # results show that it's 0.5.)
         #
         constraints.append(WordEmbeddingDistance(min_cos_sim=0.5))
-        constraints.append(MaxWordsPerturbed(max_percent=1))
-        constraints.append(LevenshteinEditDistance(5))
+        constraints.append(MaxWordsPerturbed(max_percent=0.75))
+        constraints.append(LevenshteinEditDistance(50))
         #
         # Only replace words with the same part of speech (or nouns with verbs)
         #
